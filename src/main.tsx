1fa2fe8298e7534b052b215e0d5077a3c2d3b536
MatheusFunabashi <funabashimatheus@gmail.com>
2025-12-26 15:18:44 +0100

    tsx file changed

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 
 async function enableMocking() {
-  if (!import.meta.env.DEV) return;           // ✅ only in dev
+  if (!import.meta.env.DEV) return;
   if (import.meta.env.VITE_MOCK_API === "false") return;
 
   const { worker } = await import("./lib/api/mocks/browser");
   return worker.start({ onUnhandledRequest: "bypass" });
 }
+
+enableMocking().finally(() => {
+  createRoot(document.getElementById("root")!).render(<App />);
+});
